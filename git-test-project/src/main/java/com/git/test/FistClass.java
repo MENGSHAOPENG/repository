8024f0a58d12778ffe22dbc59b7e1d513a919adc
MENG <mengshaopenggg@qq.com>
2021-07-19 14:06:26 +0800

    把本地工程添加到git

---
+++ git-test-project/src/main/java/com/git/test/FistClass.java
@@ -0,0 +1,13 @@
+package com.git.test;
+
+public class FistClass {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git  ");
+    }
+}
