6017709bd6eee6ca0b64f93dcdfde467dc78c862
MENG <mengshaopenggg@qq.com>
2021-07-19 15:10:43 +0800

    添加secondclass类

---
+++ git-test-project/src/main/java/com/git/test/SecondClass.java
@@ -0,0 +1,11 @@
+package com.git.test;
+
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("hello,second");
+        System.out.println("hello,second");
+        System.out.println("hello,second");
+        System.out.println("hello,second");
+        System.out.println("hello,second");
+    }
+}
